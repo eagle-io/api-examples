18a028d189ec2d1ebad94473b1f6d3cc9f6b899a
nick <nick@argos.io>
2016-11-16 17:03:28 +1100

    api keys are now generated from account settings

--- php/images.php
+++ php/images.php
@@ -9,8 +9,8 @@
 // Include HTTPFUL Rest Client Package from http://phphttpclient.com
 include('./lib/httpful.phar');
 
-$api_key      = 'YOUR_API_KEY_HERE'; // You can generate an API key from User preferences
-$api_path     = 'https://eagle.io/api/v1/';
+$api_key      = 'YOUR_API_KEY_HERE'; // You can generate an API key from account settings
+$api_path     = 'https://api.eagle.io/api/v1/';
 $api_resource = 'nodes';
 $api_query    = 'filter=_class($match:io.eagle.models.node.Attachment)&attr=name,fileUrl,filePreviewUrl&sort=createdTime(DESC)&limit=10';
 // optionally add an expiry period in minutes for the image urls.
@@ -18,7 +18,7 @@ $api_query    = 'filter=_class($match:io.eagle.models.node.Attachment)&attr=name
 
 
 // NOTE:
-// The $api_query in the example above will return the 10 most recently uploaded attachment nodes available to the user (requires ATTACHMENT_READ permission)
+// The $api_query in the example above will return the 10 most recently uploaded attachment nodes in the account
 // and will include name, fileUrl, filePreviewUrl attributes.
 // You could also return only images underneith a specific node (ie. Location, AttachmentSource) by additionally specifying a parentId as part of the filter criteria
 // eg. filter=_class($match:io.eagle.models.node.Attachment),parentId($eq:1234567890abcdef)
