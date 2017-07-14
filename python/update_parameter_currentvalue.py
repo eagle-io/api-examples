04c3b1536a17c00a5bbcc32d7e9a662476de3eb3
nick <nick@argos.io>
2017-07-14 15:57:44 +1000

    add python api example

---
+++ python/update_parameter_currentvalue.py
@@ -0,0 +1,44 @@
+# EXAMPLE update_parameter_currentvalue
+# Updates the current value for a parameter via a customId
+# Read the API Documentation @ http://docs.eagle.io/en/latest/api/index.html
+# --------------------------------------------------------------------------
+
+# Import packages (must be pre-installed via PIP)
+import urllib
+import httplib2
+import json
+import random
+
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse
+
+# Set customId and value as required
+nodeCustomId      = '@mysensor'
+nodeCurrentValue  = random.randint(0,100) # generate random number between 0-100 for this example
+
+# Set api key and resource endpoint
+api_key           = 'YOUR_API_KEY_HERE'   # you can generate an API key from account settings
+api_path          = 'http://localhost:8080/api/v1/'
+api_resource      = 'nodes/' + nodeCustomId + '/historic/now'
+
+# Build http request
+uri               = api_path + api_resource
+headers           = {'Content-Type': 'application/json', 'X-Api-Key': api_key}
+body              = {'value': nodeCurrentValue}  # optionally include timestamp and quality. eg. {'value': 15, 'timestamp': '2017-07-14T23:38:00Z', 'quality": 149}
+
+# Send http request and get response
+http              = httplib2.Http()
+response, content = http.request(uri, 'PUT', json.dumps(body), headers)
+
+# Parse return content as json
+data              = json.loads(content)
+
+# Ensure we get a 202 Accepted response (as per docs)
+if response.status != 202:
+    print str(response.status) + ': ' + data['error']['message']
+    exit()
+
+# Pretty print the response payload
+print json.dumps(data, indent=4)
