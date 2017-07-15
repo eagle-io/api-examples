7e47bfd91eb8f7edb4abadfd996343cd26e3fcd2
nick <nick@argos.io>
2017-07-15 10:30:59 +1000

    fix typo

--- python/update_parameter_currentvalue.py
+++ python/update_parameter_currentvalue.py
@@ -1,5 +1,5 @@
 # EXAMPLE update_parameter_currentvalue
-# Updates the current value for a parameter via a customId
+# Updates the current value for a parameter via its customId
 # Read the API Documentation @ http://docs.eagle.io/en/latest/api/index.html
 # --------------------------------------------------------------------------
 
@@ -20,19 +20,19 @@ nodeCurrentValue  = random.randint(0,100) # generate random number between 0-100
 
 # Set api key and resource endpoint
 api_key           = 'YOUR_API_KEY_HERE'   # you can generate an API key from account settings
-api_path          = 'http://localhost:8080/api/v1/'
+api_path          = 'https://api.eagle.io/api/v1/'
 api_resource      = 'nodes/' + nodeCustomId + '/historic/now'
 
 # Build http request
 uri               = api_path + api_resource
 headers           = {'Content-Type': 'application/json', 'X-Api-Key': api_key}
-body              = {'value': nodeCurrentValue}  # optionally include timestamp and quality. eg. {'value': 15, 'timestamp': '2017-07-14T23:38:00Z', 'quality": 149}
+body              = {'value': nodeCurrentValue}  # optionally include timestamp and quality. eg. {'value': 15, 'timestamp': '2017-07-14T23:38:00Z', 'quality': 149}
 
 # Send http request and get response
 http              = httplib2.Http()
 response, content = http.request(uri, 'PUT', json.dumps(body), headers)
 
-# Parse return content as json
+# Parse content as json
 data              = json.loads(content)
 
 # Ensure we get a 202 Accepted response (as per docs)
